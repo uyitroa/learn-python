f5484ae161a7515dce109bb027653ee1f970dc77
Your Name <you@example.com>
2015-08-19 10:16:51 +0200

    Đoán chữ

--- doanchu.py
+++ doanchu.py
@@ -1,12 +1,45 @@
+# Guess a word
+# Import library
 import random
-print("Welcome to the guessing word.")
-def play(a,b,c,d,e):
-	word=input()
-	if word=='a':
-		print("a_ _ _ _")	
-	
-def guess(th):
-	print("The secret word have "+str(th)+" lettre)
-	print("So, guess a lettre or the secret word")
-	
-		
+# Make list of word
+Word=['buy','dolphin','machine','mouse','heart','lamp','clock','table','key','water','fire','orange','house','horse']
+choisir=input("Do you to play(yes)? ")
+if choisir=='yes':
+	choose=random.choice(Word)
+	print(str(choose))
+# The length of the secret word
+	blanks='_ '*len(choose)
+	print("Secret word: "+str(blanks))
+#Guess the letter
+	guess=input("Guess the letter of the secret word: ")
+# Make a loop infinity
+x=100000000000000000000
+# Make the time for count
+counter=0
+def whi(guess):	
+	while len(guess)>1 or guess not in 'qwertyuiopasdfghjklzxcvbnm':
+		if len(guess)>1:
+			print("Guess only a letter in one time.")
+		if guess not in 'qwertyuiopasdfghjklzxcvbnm':
+			print("Guess only the letter and no other.")
+		guess=input("Guess a letter of the secret word: ")
+	return guess
+while choisir=='yes':
+	whi(guess)
+	for letter in range(x):
+		whi(guess)
+		if guess in choose:
+			if counter==len(choose)-1:
+				print("Congratulation!You won!")
+				break
+			counter+=1
+			letterIndex=choose.index(guess)
+			newBlanks=blanks[:letterIndex*2] + guess + blanks[letterIndex*2+1:]
+			newBlanks=blanks
+			blanks=blanks[:letterIndex*2] + guess + blanks[letterIndex*2+1:]
+			print("Secret word: "+str(blanks))
+		else:
+			print("It's wrong!")
+		guess=input("Guess again the letter: ")
+	choisir=input("Do you to play again(yes)? ")
+
