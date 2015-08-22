01032ecd8b49ed3520f36ecf26c457370b030528
Your Name <you@example.com>
2015-08-22 10:23:46 +0200

    Soccer penalty 2

---
+++ penalty2.py
@@ -0,0 +1,29 @@
+# Game of goal
+# Import library
+import random
+far=[' o                                            ','  o                                           ','   o                                          ','    o                                         ','     o                                        ','         o                                    ','          o                                   ','                  o                           ']
+guess=random.choice(far)
+blanks=' '*len(far)
+newBlanks=blanks + guess + ' ¶  ||||'
+o=guess.index('o')
+goals=newBlanks.count(" ",o, len(newBlanks))
+goals*=1.3
+print(str(goals))
+def goal():
+	print("\t\t\t\t\t\t\t–\++\ ")
+	print(newBlanks)
+	print("\t\t\t\t\t\t\t–/++/")
+goal()
+foot=input("Each charater is a space of the ball, so enter the legth of the charater for shoot the ball.")
+newBlanks=blanks + ' '*len(foot) + guess + '\b' * len(foot) + ' ¶  ||||'
+goal()
+o=newBlanks.index('o')
+ball=newBlanks.count(" ",0,o)
+ball*=1.3
+print(str(ball))
+if (goals - 3 <= ball <= goals + 3):
+	print("GOALS! You have a goal from "+str(ball)+" meters")
+elif ball < goals:
+	print("It's very weak, try again with more strong.")
+else:
+	print("Opps, the goalie was stop the ball flew into the goal.")
