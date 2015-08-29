b7d13e016bd3dcae1f181de67a69bef980d95fe5
Your Name <you@example.com>
2015-08-29 09:21:11 +0200

    Nó báo là lỗi của em ở đây ạ: TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'

--- GameofHung/chiecnon4.py
+++ GameofHung/chiecnon4.py
@@ -3,20 +3,62 @@
 # Import library
 import random
 import time
+# Set
+# Make the time for count
+counter = 0
+# Point of first player
+count1 = 0
+# Point of second player
+count2 = 0
+# Set a turn
+turn = 'first' 
+# Make list of word
+Word=['book','ball','door','football','game','buy','dolphin','machine','mouse','heart','lamp','clock','table','key','water','fire','orange','house','horse']
 
-# Make function
+# Make functions.
+
+# Random for choose the secret word
+def chooseWord():
+	choose = random.choice(Word)
+	choose = 'football'
+	return choose
+
+# Random point
+def turnNon():	
+	# Random the point for each correct guess.
+	non = random.randint(1,13)
+	#  Print the point of the player.
+	print("You are having 0 point.")
+	print("Turn of the first player\n")
+	print("Turning...")
+	t(2)
+	if non<=9:
+		# If the player has a correct answer, he will have current points + non
+		print("You will maybe have "+str(non*100)+" points")
+	elif non==13:
+		non = 0
+		# If non == 13, the player was lost his turn.
+		turn='second'
+		print("Opps, you was lost your turn. It's second player's turn.")
+	else:
+		# If non == 10(<=> current point * 2 <=> 0 * 2 == 0), non == 11(<=> current point /2 <=> 0 / 2 == 0), non == 12(<=> current point == 0) so not thing change.
+		print("Not thing change.")
+		non = 0
+	return non
 
 # Calcul the point of the player
 def score(currentPoint,t_value):
 	if t_value == 2:
-		currentPoint *= 2
+		currentPoint = currentPoint * 2
 	else:
-		currentPoint += t_value
+		currentPoint = currentPoint + t_value
+	
 	return currentPoint
-
+print(str(score(100,200)))
 # Print the point of the player
 def currentPoint(cont):
 	print("You are having ",cont," points")
+	return cont
 
 # Print the player will or was have points, double points, half of points or lost all points.
 def playPoint(coi,tun):
@@ -31,9 +73,10 @@ def playPoint(coi,tun):
 	elif non == 13:
 		turn = tun
 		print("Opps, you was lost your turn. It's of the "+ tun +" player.")
+	return coi
 
 # While the player win
-def playWin(con,pri):
+def playWin(con,pri,cou):
 	print ("Congratulation! You won! You have "+str(con)+" points. The " + pri +" player has "+str(cou)+" points.")
 
 # The time for random.
@@ -42,20 +85,21 @@ def t(s):
 
 # Check if the player input is a letter or not
 def check(guess):
-	while len(guess)>1 or guess not in 'qwertyuiopasdfghjklzxcvbnm' or guess == "" or blanks.find(guess,0,len(blanks)) != -1:
-		# It's string.
-		if len(guess)>1:
-			print("Guess only a letter in one time.")
-		# It's not a letter.
-		if guess not in 'qwertyuiopasdfghjklzxcvbnm' or guess == "":
-			print("Guess only the letter and no other.")
-		# It's has guessed.
-		if blanks.find(guess,0,len(blanks)) != -1:
-			print("Guess only the letter hasn't guess.")
-		guess = input("Guess a letter of the secret word: ")
-		return guess
+	# It's a string.
+	if len(guess)>1:
+		print("Guess only a letter in one time.")
+	# It's not a letter.
+	if guess not in 'qwertyuiopasdfghjklzxcvbnm' or guess == "":
+		print("Guess only the letter and no other.")
+	# It's has guessed.
+	if blanks.find(guess,0,len(blanks)) != -1:
+		print("Guess only the letter hasn't guess.")
+	guess = input("Guess a letter of the secret word: ")
+	return guess
+
 # If the player has a correct guess
-def guessCorrect():
+def guessCorrect(non):
+	global count1,count2,counter,blanks
 	# If the turn is of the first player. 
 	if turn == 'first':
 		if non<=9:
@@ -63,7 +107,6 @@ def guessCorrect():
 			count1 = score(count1,non)
 		elif non==10:
 			count1 = score(count1,2)
-		currentPoint(count1)
 	# If the turn is of the second player.
 	else:
 		if non<=9:
@@ -71,7 +114,6 @@ def guessCorrect():
 			count2 = score(count2,non)
 		elif non==10:
 			count2 = score(count2,2)
-		currentPoint(count2)
 	m = -1
 	# Check the word has x letter's the player haas guessed.
 	for fi in range(len(choose)):
@@ -90,10 +132,12 @@ def guessCorrect():
 		else:
 			playWin(count2,"first",count1)
 		return
+
 # If the player has a incorrect guess
-def guessIncorrect():
+def guessIncorrect(turn):
 	print("It's wrong!")
 	print("You don't have any point.")
+	print("Secret word: ",blanks)
 	if turn == 'first': # The turn is of the fist player, change the turn for the second player.
 		print("It's the turn of the second player.")
 		turn = 'second'
@@ -101,42 +145,6 @@ def guessIncorrect():
 		print("It's the turn of the first player.")
 		turn = 'first'
 	return turn
-
-# Make list of word
-Word=['book','ball','door','football','game','buy','dolphin','machine','mouse','heart','lamp','clock','table','key','water','fire','orange','house','horse']
-# Random for choose the secret word
-
-def chooseWord():
-	choose = random.choice(Word)
-	choose = 'football'
-	# Random the point for each correct guess.
-	non = random.randint(1,13)
-	#  Print the point of the player.
-	print("Turn of the first player\n")
-	print("You are having 0 point")
-	print("Turning...")
-	t(2)
-	# Make the time for count
-	counter = 0
-	# Point of first player
-	count1 = 0
-	# Point of second player
-	count2 = 0
-	# Set a turn
-	turn = 'first'
-	if non<=9:
-		# If the player has a correct answer, he will have current points + non
-		print("You will maybe have "+str(non*100)+" points")
-	elif non==13:
-		non = 0
-		# If non == 13, the player was lost his turn.
-		turn='second'
-		print("Opps, you was lost your turn.")
-	else:
-		# If non == 10(<=> current point * 2 <=> 0 * 2 == 0), non == 11(<=> current point /2 <=> 0 / 2 == 0), non == 12(<=> current point == 0) so not thing change.
-		print("Not thing change.")
-		non = 0
-	return choose
 
 # The blank '_ _ _ _ _ ' of the secret word
 def my_Blanks():
@@ -155,36 +163,53 @@ def secretGuess():
 
 playAgain = 'yes'
 while playAgain == 'yes':
-	
+	# Reset
+	# Set
+	# Make the time for count
+	counter = 0
+	# Point of first player
+	count1 = 0
+	# Point of second player
+	count2 = 0
+	# Set a turn
+	turn = 'first'
+	print(str(count1))
 	# Use function
 	choose = chooseWord()
+	non = turnNon()
 	blanks = my_Blanks()
 	guess = secretGuess()
-	
-	# Make a loop infinity
+	print(str(count1))
+	# Make a infinty loop
 	while 0 != 1:
-		
-		guess = check(guess)
+		# Check guess
+		while len(guess)>1 or guess not in 'qwertyuiopasdfghjklzxcvbnm' or guess == "" or blanks.find(guess,0,len(blanks)) != -1:
+			guess = check(guess)
 		
 		# If the player has a correct guess.
+		guess = str(guess)
 		if guess in choose:
-			guessCorrect()
-		
+			if turn == 'first':
+				count1 = guessCorrect(non)
+			else:
+				count2 = guessCorrect(non)
+			print(str(score(count1,non)))
+			print(count1, count2)
 		# If the player has incorrect guess.
 		else:
-			guessIncorrect()
-		
+			turn  = guessIncorrect(turn)
+			print(count1, count2)
 		# Quay chiếc nón
-		non = random.randint(1,9)
+		non = random.randint(1,13)
 		# The turn is of the first player
 		
 		if turn == 'first':
 			currentPoint(count1)
-		
+			print(str(count1))
 		# The turn is of the second player
 		else:
 			currentPoint(count2)
-		
+			print(str(count2))
 		print("Turning...")
 		t(2)
 		
@@ -193,11 +218,10 @@ while playAgain == 'yes':
 		
 		elif turn == 'first':
 			count1 = playPoint(count1,'second')
-		
+			print(str(count1))
 		elif turn == 'second':
 			count2 = playPoint(count2,'first')
-		
+			print(str(count2))
 		guess = secretGuess()
-
 	# Ask the player if he want to play again or not.
 	playAgain = input("Play again? (yes or no): ")
