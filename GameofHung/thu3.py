09c0c020ec355370b766fa1b91974b303c3ef8b5
Your Name <you@example.com>
2015-09-12 21:27:24 +0200

    Em sửa đc hai vấn đề của bài tách chữ

--- GameofHung/thu3.py
+++ GameofHung/thu3.py
@@ -12,6 +12,14 @@ lis = lis.replace(","," ")
 	
 lis = lis.replace("\n"," ")
 
+for q in range(10):
+	
+	q = str(q)
+
+	if lis.find(q) != -1:
+
+		lis = lis.replace(q," ")
+
 print("Read this text and search a secret word:\n"+str(lis))
 
 mylist = lis.split(" ")
@@ -27,6 +35,8 @@ for a in range(len(mylist)):
 			dif.remove(mylist[a])
 		
 			break
+
+dif.remove(dif[len(dif) - 1])
 
 print(str(dif))
 
