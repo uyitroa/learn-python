17b3257cc2cbeac1a37f71aacafa5b54a1bd2187
Your Name <you@example.com>
2015-11-22 11:17:36 +0100

    GAME BOMB

--- game.py
+++ game.py
@@ -1,19 +1,54 @@
+import pygame,sys
+from pygame import *
+import time
 import random
-x=random.randint(0,100)
-print("The secret number is between 0 and 100.")
-print("Input the secret number")
-a=input()
-a=int(a)
-counter=1
-while a!=x:
-	counter=counter+1
-	print("Please try again")
-	if a>x:
-		print("The secret number is smaller than "+ str(a))
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 128, 0)
+YELLOW = (255, 255, 0)
+DISPLAYSURF = pygame.display.set_mode((800,800))
+pygame.display.set_caption('Hello World!')
+bomb = pygame.image.load('bomb.png')
+car = pygame.image.load('car.png')
+x = 400
+y = 700
+xposition = 400
+yposition = 0
+fpsClock = pygame.time.Clock()
+explo = pygame.image.load('ex.png')
+explosion = False
+while True:
+	if yposition > 680:
+		yposition = 0
 	else:
-		print("The secret number is bigger than "+ str(a))
-	print("Input the secret number")
-	a=input()
-	a=int(a)
-print("Congrastulation. You have my number after guessing "+str(counter)+ " times")
+		yposition += 5
+	if yposition != 680:
+		img = bomb
+	else:
+		explosion = True
+		img = explo
+	DISPLAYSURF.fill(WHITE)
+	if explosion == True:
+		yposition -= 50
+		xposition = random.randint(100,700)
+	DISPLAYSURF.blit(img,(xposition,yposition))
+	DISPLAYSURF.blit(car,(x,y))
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		elif event.type == KEYDOWN:
+			if event.key == K_LEFT:
+				x -= 10
+			if event.key == K_RIGHT:
+				x += 10
+	pygame.display.update()
+	fpsClock.tick(30)
+	if explosion == True:
+		explosion = False
+		time.sleep(1)
+		xposition = 400
+		yposition = 0
 
