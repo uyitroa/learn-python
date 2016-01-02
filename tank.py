3f83052551be4a5f737c09f1762f8358862882e8
Your Name <you@example.com>
2016-01-02 16:27:48 +0100

    Update tank

--- tank.py
+++ tank.py
@@ -1,7 +1,23 @@
-import pygame,sys
+import pygame,sys,os,string
 from pygame import *
 import time
 import random
+# account,level,unlock,exp,expmax
+account = open("info.doc", "r")
+accoun = account.read()
+na = str(accoun).split(':')
+info = []
+name = []
+rest = []
+for s in range(len(na)):
+	if s % 2 == 0:
+		name.append(na[s])
+	else:
+		rest.append(na[s])
+na = str(accoun).split(';')
+for s in range(len(na)):
+	if s != 0:
+		info.append(na[s])
 pygame.init()
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
@@ -54,9 +70,12 @@ dired = 'left'
 touch = False
 show2 = True
 countw = 0
+modeh = False
 quit = False
 counth = 100
 weapon = False
+unlocked = [1]
+locked = False
 def tankex(x,y):
 	pygame.draw.rect(DISPLAYSURF, BLACK, (x,y, 50 , 50))
 	pygame.draw.rect(DISPLAYSURF, BLACK, (x+60,y, 50 , 50))
@@ -184,8 +203,9 @@ def helpme():
 		if mousex >= 10 and mousex <= 70 and mousey >= 740 and mousey <= 755:
 			help = False
 
+locked = False
 def cos():
-	global changec,clicked,costume
+	global changec,clicked,costume,locked
 	DISPLAYSURF.fill(WHITE)
 	font('QUIT',BLACK,WHITE,35,750,32)
 	font('CUSTOM',BLACK,WHITE, 425, 375,32)
@@ -206,15 +226,38 @@ def cos():
 				costume -= 1
 			clicked = False
 		if mousex >= 10 and mousex <= 70 and mousey >= 740 and mousey <= 755:
-			changec = False
+			if locked == False:
+				changec = False
 	if costume == 1:
 		tank2(x2,y2)
+		locked = False
 	elif costume == 2:
 		tank2c2(x2,y2)
+		for s in range(len(unlocked)):
+			if perlevel >= 2:
+				locked = False
+				break
+			else:
+				font('Locked! Level 2 will unlock',BLACK,WHITE,x2,y2 - 50,32)
+				locked = True
 	elif costume == 3:
 		tank2c3(x2,y2)
+		for s in range(len(unlocked)):
+			if perlevel >= 3:
+				locked = False
+				break
+			else:
+				font('Locked! Level 3 will unlock',BLACK,WHITE,x2,y2 - 100,32)
+				locked = True
 	elif costume == 4:
 		tank2c4(x2,y2)
+		for s in range(len(unlocked)):
+			if perlevel >= 4:
+				locked = False
+				break
+			else:
+				font('Locked! Level 4 will unlock',BLACK,WHITE,x2,y2 - 50,32)
+				locked = True
 def heavy():
 	global xh, yh, x2, life2,listxc, listyc,counth,just, listsp, listy
 	recta(xh,yh)
@@ -380,12 +423,13 @@ def lose():
 		counttime2 = 50
 		life2 = 5
 def win():
-	global counttime, life
+	global counttime, life, exp
 	counttime -= 1
 	font('You Win!',BLACK, WHITE, 350,375,32)
 	if counttime == 0:
 		life = 5
 		counttime = 50
+		exp += 2
 def pcmove():
 	global dir, x, x4, touch
 	if dir == 'right':
@@ -472,6 +516,49 @@ def level2():
 			DISPLAYSURF.blit(explosion,(xlist[c] - 50,ylist[c] - 100))
 	if bumlevel == True:
 		pcban2()
+def bartype(x,y):
+	pygame.draw.rect(DISPLAYSURF, WHITE, (x,y, 200 , 25))
+def replace(file, search_for, replace_with):
+	# replace strings in a text file
+	back = os.path.splitext(file)[0] + ".bak"
+	temp = os.path.splitext(file)[0] + ".tmp"
+	try:
+		# remove old temp file, if any
+		os.remove(temp)
+	except os.error:
+		pass
+	fi = open(file)
+	fo = open(temp, "w")
+	for s in fi.readlines():
+		fo.write(str.replace(s, search_for, replace_with))
+	fi.close()
+	fo.close()
+	try:
+	# remove old backup file, if any
+		os.remove(back)
+	except os.error:
+		pass
+	# rename original to backup...
+	os.rename(file, back)
+	# ...and temporary to original
+	os.rename(temp, file)
+def tkmk():
+	global info, name, rest
+	account = open("info.doc", "r")
+	accoun = account.read()
+	na = str(accoun).split(':')
+	info = []
+	name = []
+	rest = []
+	for s in range(len(na)):
+		if s % 2 == 0:
+			name.append(na[s])
+		else:
+			rest.append(na[s])
+	na = str(accoun).split(';')
+	for s in range(len(na)):
+		if s != 0:
+			info.append(na[s])
 bumlevel = False
 ylist = []
 xlist = []
@@ -494,14 +581,149 @@ pressed = False
 level = 1
 modest = False
 modec = False
+letteraccount = ''
+passaccount = ''
+login = False
+typepass = False
+typeletter = False
+letterac = ''
+finish = False
+perlevel = 1
+exp = 0
+expmax = 10
+printlogin = True
+printre = True
+defaut = ''
+loged = False
+restr = False
+already = False
+timemachine = 50
+passe = False
+logout = True
+just = True
 while True:
 	DISPLAYSURF.fill(BLACK)
+	if printlogin == True:
+		font('LOG IN',BLACK,WHITE,700, 50,32)
+	else:
+		font('Welcome back '+str(name[defaut])+'!',WHITE,BLACK,450,50,24)
+	if loged == True:
+		font('Log out',BLACK,WHITE,700,50,32)
 	font('PLAY',BLACK,WHITE,375,375,32)
 	font('CUSTOM',BLACK,WHITE,375,425,32)
 	font('HELP',BLACK,WHITE,375,475,32)
+	font('Level: '+str(perlevel),WHITE,BLACK,100,50,32)
+	font('Save',BLACK,WHITE,375,700,32)
+	if printlogin != False:
+		font('REGISTER',BLACK,WHITE,550,50,32)
+	if login == True:
+		tkmk()
+		restr = False
+		if printlogin == True:
+			bartype(600,100)
+		if clicked == True:
+			if mousex >= 450 and mousex <= 750 and mousey >= 75 and mousey <= 125:
+				typeletter = True
+				letteraccount = ''
+				clicked = False
+		if typeletter == True:
+			font(str(letteraccount),BLACK,WHITE,700,110,20)
+		if finish == True:
+			for a in range(len(name)):
+				if letteraccount == name[a]:
+					#if a != 0:
+					#perlevel = info[(a*4) + 1]
+					#perlevel = int(perlevel)
+					#unlocked = info[(a*4) + 2]
+					#exp = info[(a * 4) + 3]
+					#exp = int(exp)
+					#expmax = info[(a *4) + 4]
+					#expmax = int(expmax)
+					#else:
+					perlevel = info[(a * 4)]
+					perlevel = int(perlevel)
+					unlocked = info[(a * 4) + 1]
+					exp = info[(a * 4) + 2]
+					exp = int(exp)
+					expmax = perlevel * 10
+					expmax = int(expmax)
+					defaut = a
+					printlogin = False
+					finish = False
+					loged = True
+					logout = False
+					letteraccount = ''
+					break
+	else:
+		if restr != True:
+			passaccount = ''
+			letteraccount = ''
+			finish = False
+			login = False
+	if logout == True:
+		if just == True:
+			perlevel = 1
+			unlocked = [1]
+			exp = 0
+			expmax = 10
+			loged = False
+			printlogin = True
+			letteraccount = ''
+			just = False
+	if restr == True:
+		login = False
+		if printre == True:
+			bartype(450,100)
+		if clicked == True:
+			if mousex >= 150 and mousex <= 550 and mousey >= 75 and mousey <= 125:
+				typeletter = True
+				letteraccount = ''
+				clicked = False
+		if typeletter == True:
+			font(str(letteraccount),BLACK,WHITE,550,110,20)
+		if finish == True:
+			for a in range(len(name)):
+				if letteraccount == name[a]:
+					already = True
+					passe = False
+					break
+				else:
+					already = False
+					passe = True
+		if passe == True:
+			if already == False:
+				new = accoun + ':'+str(letteraccount)+':;1;[1];0;10'
+				replace('info.doc',accoun,new)
+				finish = False
+				restr = False
+				printre = False
+				passe = False
+				
+	if already == True:
+		font("It has already!",BLACK,WHITE,375,375,70)
+		timemachine -= 1
+		if timemachine == 0:
+			timemachine = 50
+			already = False
 	if clicked == True:
 		if mousex >= 325 and mousex <= 425 and mousey >= 350 and mousey <= 400:
 			start = True
+		if mousex >= 650 and mousex <= 750 and mousey >= 25 and mousey <= 75:
+			if logout == True:
+				if login == False:
+					login = True
+				else:
+					login = False
+			else:
+				logout = True
+				just = True
+			clicked = False
+		if mousex >= 500 and mousex <= 600 and mousey >= 25 and mousey <= 75:
+			if restr == False:
+				restr = True
+			else:
+				restr = False
+			clicked = False
 	if clicked == True:
 		if mousex >= 275 and mousex <= 450 and mousey >= 400 and mousey <= 450:
 			changec = True
@@ -517,8 +739,17 @@ while True:
 		modec = True
 	else:
 		modec = False
+	if mousex >= 325 and mousex <= 425 and mousey >= 675 and mousey <= 725:
+		font('Save',BLACK,WHITE,375,700,45)
+		if clicked == True:
+			if loged == True:
+				old = name[(defaut)] +':;' + info[(defaut * 4) + 0] + ';' + info[(defaut * 4) + 1] + ';' + info[(defaut * 4) + 2] + ';' + info[(defaut * 4) + 3]
+				new = name[defaut] + ':;'+ str(perlevel) + ';'+ unlocked + ';'+ str(exp) + ';'+ str(expmax)
+				replace('info.doc',old,new)
 	if modec == True:
 		font('CUSTOM',BLACK,WHITE,375,425,45)
+	if mousex >= 325 and mousex <= 450 and mousey >= 475 and mousey <= 525:
+		font('HELP',BLACK,WHITE,375,475,45)
 	if help == True:
 		helpme()
 	if changec == True:
@@ -541,7 +772,7 @@ while True:
 			permove()
 			perfire()
 		if mousex >= 730 and mousex <= 775 and mousey >= 390 and mousey <= 415:
-			if click == True:
+			if clicked == True:
 				reset()
 		if life <= 0:
 			win()
@@ -549,6 +780,10 @@ while True:
 			show2 = True
 			if level == 1:
 				level = 2
+		if exp >= expmax:
+			perlevel += 1
+			exp = 0
+			expmax += 10
 		pcmove()
 		if level == 1:
 			level1()
@@ -558,7 +793,7 @@ while True:
 			level2()
 			if life != 0:
 				tankc2(x,y)
-		if life2 == 0:
+		if life2 <= 0:
 			lose()
 			tankex(x2,y2)
 		move()
@@ -588,26 +823,26 @@ while True:
 			can2 = True
 		if special == True:
 			specialattack()
-	if countw != 0:
-		countw -= 1
-		weapon = False
-	if weapon == True:
-		if countw <= 0:
-			specialweapon()
-	if coldtime != 0:
-		coldtime -= 1
-	if lend != 0:
-		lend -= 1
-	if counth != 0:
-		counth -= 1
-		if counth == 0:
-			just = True
-	if counth == 0:	
-		if just == True:
-			xh = x
-			yh = y
-			just = False
-		heavy()
+		if countw != 0:
+			countw -= 1
+			weapon = False
+		if weapon == True:
+			if countw <= 0:
+				specialweapon()
+		if coldtime != 0:
+			coldtime -= 1
+		if lend != 0:
+			lend -= 1
+		if counth != 0:
+			counth -= 1
+			if counth == 0:
+				just = True
+		if counth == 0:	
+			if just == True:
+				xh = x
+				yh = y
+				just = False
+			heavy()
 	for event in pygame.event.get():
 		if event.type == 4:
 			mousex, mousey = event.pos
@@ -651,7 +886,96 @@ while True:
 			else:
 				fie = False
 				pressed = False
+			if event.key == K_a:
+				letteraccount += 'a'
+				passaccount += 'a'
+			if event.key == K_b:
+				letteraccount += 'b'
+				passaccount += 'b'
+			if event.key == K_c:
+				letteraccount += 'c'
+				passaccount += 'c'
+			if event.key == K_d:
+				letteraccount += 'd'
+				passaccount += 'd'
+			if event.key == K_e:
+				letteraccount += 'e'
+				passaccount += 'e'
+			if event.key == K_f:
+				letteraccount += 'f'
+				passaccount += 'f'
+			if event.key == K_g:
+				letteraccount += 'g'
+				passaccount += 'g'
+			if event.key == K_h:
+				letteraccount += 'h'
+				passaccount += 'h'
+			if event.key == K_i:
+				letteraccount += 'i'
+				passaccount += 'i'
+			if event.key == K_k:
+				letteraccount += 'k'
+				passaccount += 'k'
+			if event.key == K_l:
+				letteraccount += 'l'
+				passaccount += 'l'
+			if event.key == K_m:
+				letteraccount += 'm'
+				passaccount += 'm'
+			if event.key == K_n:
+				letteraccount += 'n'
+				passaccount += 'n'
+			if event.key == K_o:
+				letteraccount += 'o'
+				passaccount += 'o'
+			if event.key == K_p:
+				letteraccount += 'p'
+				passaccount += 'p'
+			if event.key == K_q:
+				letteraccount += 'q'
+				passaccount += 'q'
+			if event.key == K_r:
+				letteraccount += 'r'
+				passaccount += 'r'
+			if event.key == K_s:
+				letteraccount += 's'
+				passaccount += 's'
+			if event.key == K_t:
+				letteraccount += 't'
+				passaccount += 't'
+			if event.key == K_u:
+				letteraccount += 'u'
+				passaccount += 'u'
+			if event.key == K_v:
+				letteraccount += 'v'
+				passaccount += 'v'
+			if event.key == K_w:
+				letteraccount += 'w'
+				passaccount += 'w'
+			if event.key == K_x:
+				letteraccount += 'x'
+				passaccount += 'x'
+			if event.key == K_y:
+				letteraccount += 'y'
+				passaccount += 'y'
+			if event.key == K_z:
+				letteraccount += 'z'
+				passaccount += 'z'
+			if event.key == 8:
+				for s in range(len(letteraccount)):
+					if s == len(letteraccount) - 1:
+						break
+					letterac += letteraccount[s]
+				letteraccount = letterac
+			else:
+				letterac = ''
+			if event.key == 13:
+				finish = True
+			else:
+				finish = False
 		elif event.type != KEYDOWN:
 			dir2 = 'shit'
 			fie = False
+			finish = False
+			letterac = ''
 	pygame.display.update()
